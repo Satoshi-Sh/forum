a645fd22e8bef55ad0d21106a9cee2e2d5817ef2
satoshi-sh <satoss1108@gmail.com>
2025-09-12 15:27:59 -0500

    Finished ep12

--- resources/js/Pages/Posts/Index.vue
+++ resources/js/Pages/Posts/Index.vue
@@ -5,7 +5,7 @@
                 <li v-for="post in posts.data" :key="post.id">
                     <Link :href="route('posts.show', post.id)" class="block group px-2 py-4">
                         <span class="font-bold text-lg group-hover:text-indigo-500">{{ post.title }}</span>
-                        <span class="block mt-1 text-sm text-gray-600">{{ formattedDate(post) }} by {{
+                        <span class="block mt-1 text-sm text-gray-600">{{ relativeDate(post.created_at) }} by {{
                                 post.user.name
                             }}</span>
                     </Link>
@@ -21,12 +21,9 @@ import AppLayout from "@/Layouts/AppLayout.vue";
 import Container from "@/Components/Container.vue";
 import Pagination from "@/Components/Pagination.vue";
 import {Link} from "@inertiajs/vue3";
-import {formatDistance, parseISO} from "date-fns";
+import {relativeDate} from "@/utils/date.js"
 
 defineProps(['posts']);
 
-const formattedDate = (post) => {
-    return formatDistance(parseISO(post.created_at), new Date(), {addSuffix: true})
-};
 
 </script>
--- resources/js/Pages/Posts/Show.vue
+++ resources/js/Pages/Posts/Show.vue
@@ -2,10 +2,19 @@
     <AppLayout :title="post.title">
         <Container>
             <h1 class="text-2xl font-bold">{{ post.title }}</h1>
-            <span class="mt-1 text-sm text-gray-600">{{ formattedData }} by {{ post.user.name }}</span>
+            <span class="mt-1 text-sm text-gray-600">{{ formattedDate }} by {{ post.user.name }}</span>
             <article class="m-6">
                 <pre class="whitespace-pre-wrap font-sans">{{ post.body }}</pre>
             </article>
+            <div class="mt-12">
+                <h2 class="font-bold text-lg">Comments</h2>
+                <ul class="divide-y mt-4">
+                    <li v-for="comment in comments.data" :key="comment.id" class="px-2 py-4">
+                        <Comment :comment="comment"/>
+                    </li>
+                </ul>
+                <Pagination :meta="comments.meta"/>
+            </div>
         </Container>
 
     </AppLayout>
@@ -13,9 +22,11 @@
 <script setup lang="ts">
 import AppLayout from "@/Layouts/AppLayout.vue";
 import {computed} from "vue";
-import {formatDistance, parseISO} from "date-fns";
+import {relativeDate} from "@/utils/date.js"
 import Container from "@/Components/Container.vue";
+import Pagination from "@/Components/Pagination.vue";
+import Comment from "@/Components/Comment.vue";
 
-const props = defineProps(['post'])
-const formattedData = computed(() => formatDistance(parseISO(props.post.created_at), new Date(), {addSuffix: true}));
+const props = defineProps(['post', 'comments'])
+const formattedDate = computed(() => relativeDate(props.post.created_at));
 </script>

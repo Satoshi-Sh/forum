45cc8b1a2a2888e4c367bdc2085bf017f7d9b8f9
satoshi-sh <satoss1108@gmail.com>
2025-09-25 20:34:04 -0500

    Finished the seriesegit add .

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,11 +10,13 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Support\Str;
+use Laravel\Scout\Searchable;
 
 class Post extends Model
 {
     use HasFactory;
     use ConvertsMarkdownToHtml;
+    use Searchable;
 
 
     public function user(): BelongsTo
